ffaa57c03aa57732927e82597c74fbcda155e145
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2022-10-13 15:45:17 -0400

    commit tree 77a369eaa662555d7d312400c07d15cb81d8dbea

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,19 @@
-class Dog
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database: "db/students.sqlite"
+  )
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+class Dog < ActiveRecord::Base
 end
